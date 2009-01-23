4ee1672c02b526daaf5fd35275bb8014dee09024
cdanzmann <cdanzmann@2692a004-e60a-11dd-a6ea-a32d6b26a33d>
2009-01-23 10:52:07 +0000

    delete bin folder

--- trunk/bin/win32 starter.bat
+++
@@ -1,4 +0,0 @@
-cd data
-java -Xss2m -Xms512m -Xmx512m -Djava.library.path="native/win32" -cp lib/audiolib/vorbisspi1.0.3.jar;lib/audiolib/tritonus_share.jar;lib/audiolib/jorbis-0.0.15.jar;lib/audiolib/jogg-0.0.7.jar;lib/jogl.jar;lib/lwjgl.jar;lib/lwjgl_util.jar;. main.SuperMain
-
-
